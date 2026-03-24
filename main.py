fe273c73b2eb1e16992f1f48bc9005cf99c6a383
Adit <aditdpatel05@gmail.com>
2026-03-24 16:24:28 -0500

    increased n estimators 100 -> 1000

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 from sklearn.ensemble import RandomForestClassifier
 
-rf = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+rf = RandomForestClassifier(n_estimators=1000, max_depth=10, random_state=42)
 rf.fit(X_train, y_train)
 
 y_pred = rf.predict(X_test)
